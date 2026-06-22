c40f32da326546b2e6984573ab49264c33fe98dd
Joguisam-gt <neokmgt@gmail.com>
2026-06-22 15:22:31 -0600

    Feat(noSQL): resolver ejercicio 03

---
+++ basico/ejercicios/03-personajes-rpg/resoluciones/jose-rodriguez/queries.mogodb.js
@@ -0,0 +1,31 @@
+// Asegurar uso de la base de datos correcta 
+use('campus_personajes_rpg');
+
+// --- OPERACIONES DE CONSULTA (READ) ---
+
+// Consulta 1: Filtrar personajes activos excluyendo su ID para visualizacion limpia 
+
+db.personajes.find({ activo: true}, {nombre: 1, clase: 1, nivel: 1, _id: 0});
+
+// Consulta 2: Buscar personajes con alto poder magico usando el documento anidado
+
+db.personajes.find({ "atributos.magia": { $gt: 50}});
+
+// Consulta 3: Buscar heroes que tengan al menos un objeto de rarera "Epica" dentro de su array de equipamiento
+
+db.personajes.find({ "equipamiento.rareza": "Épica" });
+
+// Actualizacion: El picaro "SlyTheif" subio de nivel, incrementando su nivel y su destreza anidada
+
+db.personajes.updateOne(
+    { nombre: "SlyThief" },
+    {
+        $inc: { nivel: 1, "atributos.destreza": 5 }
+    }
+);
+
+// --- OPERACION DE REPORTE O CLASIFICACION ---
+
+// Reporte: obtener el ranking de personajes ordenados del nivel mas alto al mas bajo 
+
+db.personajes.find().sort({ nivel: -1});
